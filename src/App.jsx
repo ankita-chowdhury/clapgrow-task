c1b8db81ed219ceb6d87aa834c61d7796da56547
ankita-chowdhury <108832208+ankita-chowdhury@users.noreply.github.com>
2024-06-02 22:28:41 +0530

    API Get Call
    Implemented API call with Axios  Library, used try catch block for error handling.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import UserList from './components/UserList'
+import axios from 'axios';
 
 function App() {
- 
+ const[userList,setUserList]=useState([]);
+
+ useEffect(()=>{
+  apiCall();
+ },[])
+
+ const apiCall = async() =>{
+  const path=`https://jsonplaceholder.typicode.com/users`;
+  try{
+    const response = await axios.get(path);
+    console.log("response",response.data);
+    setUserList(response.data);
+  }
+  catch(e){
+      console.log("api call failed",e);
+  }
+ }
 
   return (
     <>
